83b4c206212d1b67fbbf1795f7f0046fcf1bc11b
Janio Florêncio <info.janio@gmail.com>
2026-02-23 21:13:11 -0300

    deploy production

--- src/use-cases/_factories/make-fetch-order-by-id-use-case.ts
+++ src/use-cases/_factories/make-fetch-order-by-id-use-case.ts
@@ -1,8 +1,8 @@
 import { PrismaOrdersRepository } from "@/repositories/prisma/prisma-orders-repository";
-import { FetchOrderByIdUseCase } from "../orders/fetch-order-by-id-history";
+import { FetchOrderByIdHistoryUseCase } from "../orders/fetch-order-by-id-history";
 import { prisma } from "@/lib/prisma";
 
 export function makeFetchOrderByIdUseCase() {
   const ordersRepository = new PrismaOrdersRepository(prisma);
-  return new FetchOrderByIdUseCase(ordersRepository);
+  return new FetchOrderByIdHistoryUseCase(ordersRepository);
 }
