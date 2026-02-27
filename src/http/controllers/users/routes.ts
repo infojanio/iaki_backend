76b40fe7673d9d93600524475946f53393129a7c
Janio Florêncio <info.janio@gmail.com>
2026-02-27 18:55:56 -0300

    rota user

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -17,32 +17,35 @@ import { updateUserCity } from "./update-user-city";
 import { getUserStoresPoints } from "./get-user-stores-points";
 
 export async function usersRoutes(app: FastifyInstance) {
-
-  // Públicas
+  /* Rotas acessíveis para usuário não autenticado */
   app.post("/users", register);
   app.post("/sessions", authenticate);
-  app.post("/token/refresh", refresh);
-
-  // Autenticadas (cliente)
-  app.addHook("onRequest", verifyJWT);
-
-  app.get("/users/me", profile);
-  app.patch("/users/me", update);
-  app.patch("/users/me/city", updateUserCity);
-  app.get("/users/me/balance", balance);
-  app.get("/users/me/stores-with-points", getUserStoresPoints);
-}
-  /* SUPER_ADMIN
-  app.post(
-    "/users/admin",
-    { onRequest: [verifyUserRole("SUPER_ADMIN")] },
-    createAdmin,
-  );
 
   app.get(
-    "/users",
-    { onRequest: [verifyUserRole("SUPER_ADMIN")] },
-    listUsers,
+    "/users/me/stores-with-points",
+    { onRequest: [verifyJWT] },
+    getUserStoresPoints,
   );
+
+  app.patch("/users/city", { onRequest: [verifyJWT] }, updateUserCity);
+  //app.get("/users/:userId/city", getUserLocation);
+
+  app.patch("/users/:userId", { onRequest: [verifyJWT] }, update);
+  app.get("/users/:userId/address", getUserAddress);
+  app.get("/users/:userId/location", getUserLocation);
+  app.post("/token/refresh", refresh); // pega o token e atualiza
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
+  app.get("/users/profile", { onRequest: [verifyJWT] }, profileEdit);
+
+  // app.put('/users/:id', { onRequest: [verifyJWT] }, update) //atualiza o usuário
+  app.post("/users/location", createUserLocation);
+  app.get(
+    "/users/balance",
+    {
+      onRequest: [verifyJWT],
+    },
+    balance,
+  );
+
+  /* Rotas exclusivas para usuário autenticado */
 }
-*/
