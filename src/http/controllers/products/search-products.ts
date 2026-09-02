0d42d878548fbd28787df9c98aca4c5838422749
Janio Florêncio <info.janio@gmail.com>
2026-09-02 12:59:18 -0300

    pesquisa produto por nome loja

--- src/http/controllers/products/search-products.ts
+++ src/http/controllers/products/search-products.ts
@@ -1,7 +1,7 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
+
 import { makeSearchProductsUseCase } from "@/use-cases/_factories/make-search-products-use-case";
-import { makeListProductsActiveUseCase } from "@/use-cases/_factories/make-list-products-active-use-case";
 
 export async function searchProducts(
   request: FastifyRequest,
@@ -9,24 +9,36 @@ export async function searchProducts(
 ) {
   const searchQuerySchema = z.object({
     query: z.string().catch(""),
+    cityId: z.string().uuid(),
+
     page: z.coerce.number().int().positive().catch(1),
-    pageSize: z.coerce.number().int().positive().catch(5),
+
+    pageSize: z.coerce.number().int().positive().catch(24),
   });
 
-  const { query, page, pageSize } = searchQuerySchema.parse(request.query);
+  const { query, cityId, page, pageSize } = searchQuerySchema.parse(
+    request.query,
+  );
 
   const searchProductsUseCase = makeSearchProductsUseCase();
 
   const { products, total } = await searchProductsUseCase.execute({
     query,
+    cityId,
     page,
     pageSize,
   });
 
-  return reply.send({
+  const totalPages = Math.ceil(total / pageSize);
+
+  return reply.status(200).send({
     products,
-    total,
-    totalPages: Math.ceil(total / pageSize),
-    currentPage: page,
+
+    pagination: {
+      total,
+      totalPages,
+      currentPage: page,
+      pageSize,
+    },
   });
 }
